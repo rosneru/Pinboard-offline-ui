be4b46dce4939df5c6ff625a215c906163fdff31
Uwe Rosner <oscottmusic@posteo.de>
2025-02-12 15:57:16 +0100

    Separated BookmarkList view and added ItemTemplate

--- src/Logic.UI/ViewModels/MainViewModel.cs
+++ src/Logic.UI/ViewModels/MainViewModel.cs
@@ -23,23 +23,24 @@ namespace Logic.UI.ViewModels
   {
     private const string PINBOARD_FILE_NAME = "pinboard_backup.json";
 
-    [ObservableProperty] private UITools _uiTools;
     [ObservableProperty] private List<Bookmark> bookmarks = [];
     [ObservableProperty] private Bookmark _selectedBookmark;
     [ObservableProperty] private string _selectedBookmarkHtml;
+
+    [ObservableProperty] private UITools _uiTools;
 
     partial void OnSelectedBookmarkChanged(Bookmark oldValue, Bookmark newValue)
     {
       var bookmarkContent = newValue.Extended;
 
-      // Translate the '==' around '==Higlighted==' passages with
-      // into  '<mark>Higlighted</mark>'. Because this is the syntax,
-      // `Markdig` understands and renders highlighted.
+      // Translate the '==' around '==Highlighted==' passages with
+      // into  '<mark>Highlighted</mark>'. Because this is the syntax,
+      // `Markdig` understands and *does* render highlighted.
       //
       // Regex explained:
-      //  - '(?<!\=)' ensures ther's no '=' before start of match (Negative Lookbehind)
+      //  - '(?<!\=)' ensures there's no '=' before start of match (Negative Lookbehind)
       //  - '\={2}' matches exactly two '='
-      //  - '(?!\=)' ensures ther's no '=' after end of match (Negative Lookahead)
+      //  - '(?!\=)' ensures there's no '=' after end of match (Negative Lookahead)
       int i = 0;
       var bookmarkContendTranslated = new Regex(@"(?<!\=)\={2}(?!\=)")
         .Replace(bookmarkContent, m => i++ % 2 == 0 ? "<mark>" : "</mark>");
