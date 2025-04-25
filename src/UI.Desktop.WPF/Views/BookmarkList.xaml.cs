83ec09697f3f13e1b253845bab28e36eedcb1d29
Uwe Rosner <oscottmusic@posteo.de>
2025-04-25 15:16:28 +0200

    Fixed ListView behavior

--- src/UI.Desktop.WPF/Views/BookmarkList.xaml.cs
+++ src/UI.Desktop.WPF/Views/BookmarkList.xaml.cs
@@ -28,7 +28,46 @@ namespace UI.Desktop.WPF.Views
       InitializeComponent();
     }
 
-    private void BookmarkTag_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    private void BookmarksListView_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      if (sender is ListView listView)
+      {
+        // Number of rows to scroll
+        int scrollAmount = 1;
+
+        // Get the ScrollViewer of Bookmarks ListView
+        var scrollViewer = FindVisualChild<ScrollViewer>(listView);
+        if (scrollViewer != null)
+        {
+          if (e.Delta > 0)
+          {
+            scrollViewer.LineUp();
+          }
+          else
+          {
+            scrollViewer.LineDown();
+          }
+
+          // Scroll up to `scrollAmount` lines
+          for (int i = 1; i < scrollAmount; i++)
+          {
+            if (e.Delta > 0)
+            {
+              scrollViewer.LineUp();
+            }
+            else
+            {
+              scrollViewer.LineDown();
+            }
+          }
+
+          // Prevent default mouse wheel behavior
+          e.Handled = true;
+        }
+      }
+    }
+
+    private void TagsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
       if (DataContext is MainViewModel vm)
       {
@@ -62,6 +101,49 @@ namespace UI.Desktop.WPF.Views
       }
 
       return parent as T;
+    }
+
+    private void TagsListView_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      var parentListView = FindParent<ListView>((DependencyObject)sender);
+      if (parentListView != null)
+      {
+        // Forward the wheel scroll event to the parent ListView
+        var scrollViewer = FindVisualChild<ScrollViewer>(parentListView);
+        if (scrollViewer != null)
+        {
+          if (e.Delta > 0)
+          {
+            scrollViewer.LineUp();
+          }
+          else
+          {
+            scrollViewer.LineDown();
+          }
+
+          // Prevent the child ListView from processing the event
+          e.Handled = true;
+        }
+      }
+    }
+
+    private static T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+    {
+      for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+      {
+        var child = VisualTreeHelper.GetChild(obj, i);
+        if (child is T t)
+        {
+          return t;
+        }
+
+        var childOfChild = FindVisualChild<T>(child);
+        if (childOfChild != null)
+        {
+          return childOfChild;
+        }
+      }
+      return null;
     }
   }
 }
