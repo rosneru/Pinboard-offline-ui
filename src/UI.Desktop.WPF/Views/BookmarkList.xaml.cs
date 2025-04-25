51d75de971a6b07713d69a09570e89e09a242f61
Uwe Rosner <oscottmusic@posteo.de>
2025-04-25 11:14:29 +0200

    Fixed that not all clicks BookmarkList items ('Bookmarks') were detected as activation

--- src/UI.Desktop.WPF/Views/BookmarkList.xaml.cs
+++ src/UI.Desktop.WPF/Views/BookmarkList.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Logic.UI.Model;
 using Logic.UI.ViewModels;
 
 namespace UI.Desktop.WPF.Views
@@ -32,6 +34,34 @@ namespace UI.Desktop.WPF.Views
       {
         vm.BookmarkService.ToggleFilterTag(e.AddedItems[0].ToString());
       }
+    }
+
+    private void TagsListView_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+      // Suche das Parent-ListView
+      var parentListView = FindParent<ListView>((DependencyObject)sender);
+
+      if (parentListView != null)
+      {
+        var clickedItem = e.OriginalSource as FrameworkElement;
+        if(clickedItem.DataContext is Bookmark bookmark)
+        {
+          parentListView.SelectedItem = bookmark;
+        }
+      }
+    }
+
+    // Hilfsmethode, um das Parent-Element zu finden
+    private T FindParent<T>(DependencyObject child) where T : DependencyObject
+    {
+      DependencyObject parent = VisualTreeHelper.GetParent(child);
+
+      while (parent != null && !(parent is T))
+      {
+        parent = VisualTreeHelper.GetParent(parent);
+      }
+
+      return parent as T;
     }
   }
 }
