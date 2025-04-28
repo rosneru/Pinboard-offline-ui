98d11561515c3aad39c18baa672abea5e5943273
Uwe Rosner <oscottmusic@posteo.de>
2025-04-28 14:54:12 +0200

    Added a basic way to filter for `tags`

--- src/Logic.UI/DialogViewModels/FilterByTagsDialogViewModel.cs
+++ src/Logic.UI/DialogViewModels/FilterByTagsDialogViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Logic.UI.Services;
@@ -16,6 +17,8 @@ namespace Logic.UI.DialogViewModels
     [ObservableProperty] private bool? _dialogResult;
     [ObservableProperty] private IBookmarkService _bookmarkService;
 
+    [ObservableProperty] private string _tagToAdd;
+
     public FilterByTagsDialogViewModel(ISettingsService settingsService,
                                        IDialogService dialogService,
                                        IBookmarkService bookmarkService)
@@ -23,6 +26,40 @@ namespace Logic.UI.DialogViewModels
       _settingsService = settingsService;
       _dialogService = dialogService;
       BookmarkService = bookmarkService;
+    }
+
+    private bool CanExecuteAddTag()
+    {
+      return !string.IsNullOrEmpty(TagToAdd);
+    }
+
+    [RelayCommand(CanExecute = nameof(CanExecuteAddTag))]
+    private void AddTag()
+    {
+      if (BookmarkService.FilteredTags.Any(tag => tag.Equals(TagToAdd)))
+      {
+        _dialogService
+          .ShowMessageBox(this,
+                          $"The tag '{TagToAdd}' is already filtered",
+                          "Tag already added",
+                          MessageBoxButton.OK,
+                          MessageBoxImage.Information);
+      }
+
+      if (!BookmarkService.AllBookmarks.Any(
+        bm => bm.TagsArray.Any(tag => tag.Equals(
+          TagToAdd, StringComparison.OrdinalIgnoreCase))))
+      {
+        _dialogService
+          .ShowMessageBox(this,
+                          $"The tag '{TagToAdd}' not found in any bookmark.",
+                          "Tag not found",
+                          MessageBoxButton.OK,
+                          MessageBoxImage.Information);
+      }
+
+      BookmarkService.ToggleFilterTag(TagToAdd);
+      TagToAdd = "";
     }
 
     private bool CanExecuteApply()
@@ -49,5 +86,5 @@ namespace Logic.UI.DialogViewModels
     IDialogService _dialogService;
 
   }
-  
+
 }
