a1b67f2d2812e2c82b33e76ec58ce401282c1e5c
Uwe Rosner <oscottmusic@posteo.de>
2026-05-13 13:22:37 +0200

    Re-formatted

--- src/Logic.UI/ViewModels/MainViewModel.cs
+++ src/Logic.UI/ViewModels/MainViewModel.cs
@@ -14,172 +14,172 @@ using MvvmDialogs;
 
 namespace Logic.UI.ViewModels
 {
-  public partial class MainViewModel : ObservableObject
-  {
-    private static readonly MarkdownPipeline _markdownPipeline = new MarkdownPipelineBuilder()
-      .UseAdvancedExtensions()
-      .Build();
-
-    [ObservableProperty] private IUiService _uiService;
-    [ObservableProperty] private IBookmarkService _bookmarkService;
-    [ObservableProperty] private Bookmark _selectedBookmark;
-    [ObservableProperty] private string _selectedBookmarkHtml;
-    [ObservableProperty] private string _statusBarText;
-    [ObservableProperty] private ThemeType _currentTheme;
-
-    [ObservableProperty] private string _bookmarkFileDateInfo;
-    [ObservableProperty] private string _latestBookmarkDateInfo;
-
-    partial void OnSelectedBookmarkChanged(Bookmark oldValue, Bookmark newValue)
+    public partial class MainViewModel : ObservableObject
     {
-      if (newValue is null)
-      {
-        return;
-      }
+        private static readonly MarkdownPipeline _markdownPipeline = new MarkdownPipelineBuilder()
+          .UseAdvancedExtensions()
+          .Build();
 
-      var bookmarkContent = newValue!.Extended;
-      SelectedBookmarkHtml = Markdown.ToHtml(bookmarkContent, _markdownPipeline);
+        [ObservableProperty] private IUiService _uiService;
+        [ObservableProperty] private IBookmarkService _bookmarkService;
+        [ObservableProperty] private Bookmark _selectedBookmark;
+        [ObservableProperty] private string _selectedBookmarkHtml;
+        [ObservableProperty] private string _statusBarText;
+        [ObservableProperty] private ThemeType _currentTheme;
+
+        [ObservableProperty] private string _bookmarkFileDateInfo;
+        [ObservableProperty] private string _latestBookmarkDateInfo;
+
+        partial void OnSelectedBookmarkChanged(Bookmark oldValue, Bookmark newValue)
+        {
+            if (newValue is null)
+            {
+                return;
+            }
+
+            var bookmarkContent = newValue!.Extended;
+            SelectedBookmarkHtml = Markdown.ToHtml(bookmarkContent, _markdownPipeline);
+        }
+
+        public MainViewModel(
+          IDialogService dialogService,
+          IUiService uiService,
+          ISettingsService settingsService,
+          IBookmarkService bookmarkService,
+          SettingsDialogViewModel settingsDialogViewModel,
+          UpdateDialogViewModel updateDialogViewModel,
+          FilterByTagsDialogViewModel filterByTagsDialogViewModel)
+        {
+            _dialogService = dialogService;
+            UiService = uiService;
+            _settingsService = settingsService;
+            BookmarkService = bookmarkService;
+            _settingsDialogViewModel = settingsDialogViewModel;
+            _updateDialogViewModel = updateDialogViewModel;
+            _filterByTagsDialogViewModel = filterByTagsDialogViewModel;
+
+            // Initialize current theme
+            CurrentTheme = _settingsService.AppSettings.ReaderTheme;
+
+            BookmarkService.DisplayedBookmarksChanged += (sender, e) =>
+            {
+                StatusBarText = $"Displaying {BookmarkService.SelectedBookmarks.Count} " +
+                          $"of {BookmarkService.AllBookmarks.Count} loaded bookmarks.";
+            };
+        }
+
+        private bool CanExecuteOpenSelectedBookmarkUrl()
+        {
+            if (SelectedBookmark is null)
+            {
+                return false;
+            }
+
+            return !string.IsNullOrEmpty(SelectedBookmark.HRef);
+        }
+
+        [RelayCommand(CanExecute = nameof(CanExecuteOpenSelectedBookmarkUrl))]
+        private void OpenSelectedBookmarkUrl(CancelEventArgs cancelEventArgs)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "opera",
+                Arguments = SelectedBookmark.HRef,
+                UseShellExecute = true
+            });
+        }
+
+        [RelayCommand]
+        void OpenSettings()
+        {
+            var result = _dialogService.ShowDialog(this, _settingsDialogViewModel);
+            if (result == true)
+            {
+                // Update theme if it changed
+                CurrentTheme = _settingsService.AppSettings.ReaderTheme;
+            }
+        }
+
+        [RelayCommand]
+        async Task OpenUpdate()
+        {
+            _updateDialogViewModel.Initialize();
+            var success = _dialogService.ShowDialog(this, _updateDialogViewModel);
+            if (success == true)
+            {
+                await Loaded();
+            }
+        }
+
+        [RelayCommand]
+        async Task Loaded()
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            StatusBarText = $"Loading bookmarks..";
+
+            await BookmarkService.InitializeAsync(_settingsService.AppSettingsPath,
+                                                  _settingsService.PinboardFileName);
+
+            BookmarkFileDateInfo = BookmarkService.BookmarkFileDateInfo;
+            LatestBookmarkDateInfo = BookmarkService.LatestBookmarkDateInfo;
+
+            Mouse.OverrideCursor = null;
+        }
+
+        private bool CanExecuteExit()
+        {
+            // Not allowed if shutdown is already in progress.
+            return !_isAlreadyShutdown;
+        }
+
+        [RelayCommand(CanExecute = nameof(CanExecuteExit))]
+        private void Exit(CancelEventArgs cancelEventArgs)
+        {
+            if (cancelEventArgs != null)
+            {
+                // The command was probably triggered by a WindowCLose event. We
+                // cancel that WindowsClose because shutdown is done manually
+                // depending on the MessageBox result below.
+                cancelEventArgs.Cancel = true;
+            }
+
+            MessageBoxResult result = MessageBoxResult.Yes;
+            if (_settingsService.AppSettings.AskBeforeAppExit)
+            {
+                result = _dialogService.ShowMessageBox(
+                  this,
+                  "Do you really want to quit the application?",
+                  "Really exit?",
+                  MessageBoxButton.YesNo,
+                  MessageBoxImage.Warning);
+            }
+
+            if (result == MessageBoxResult.Yes)
+            {
+                // Shutdown() will trigger another invocation of this command.
+                // So we mark that the shutdown process has already started to
+                // block the re-invocation.
+                _isAlreadyShutdown = true;
+                Application.Current.Shutdown();
+            }
+        }
+
+        [RelayCommand]
+        void OpenTagFilter()
+        {
+            var success = _dialogService.ShowDialog(this, _filterByTagsDialogViewModel);
+            if (success == true)
+            {
+                // ??? await Filtered
+            }
+        }
+
+        IDialogService _dialogService;
+        ISettingsService _settingsService;
+        SettingsDialogViewModel _settingsDialogViewModel;
+        UpdateDialogViewModel _updateDialogViewModel;
+        FilterByTagsDialogViewModel _filterByTagsDialogViewModel;
+
+        bool _isAlreadyShutdown = false;
     }
-
-
-    public MainViewModel(IDialogService dialogService,
-                         IUiService uiService,
-                         ISettingsService settingsService,
-                         IBookmarkService bookmarkService,
-                         SettingsDialogViewModel settingsDialogViewModel,
-                         UpdateDialogViewModel updateDialogViewModel,
-                         FilterByTagsDialogViewModel filterByTagsDialogViewModel)
-    {
-      _dialogService = dialogService;
-      UiService = uiService;
-      _settingsService = settingsService;
-      BookmarkService = bookmarkService;
-      _settingsDialogViewModel = settingsDialogViewModel;
-      _updateDialogViewModel = updateDialogViewModel;
-      _filterByTagsDialogViewModel = filterByTagsDialogViewModel;
-
-      // Initialize current theme
-      CurrentTheme = _settingsService.AppSettings.ReaderTheme;
-
-      BookmarkService.DisplayedBookmarksChanged += (sender, e) =>
-      {
-        StatusBarText = $"Displaying {BookmarkService.SelectedBookmarks.Count} " +
-                        $"of {BookmarkService.AllBookmarks.Count} loaded bookmarks.";
-      };
-    }
-
-    private bool CanExecuteOpenSelectedBookmarkUrl()
-    {
-      if (SelectedBookmark is null)
-      {
-        return false;
-      }
-
-      return !string.IsNullOrEmpty(SelectedBookmark.HRef);
-    }
-
-    [RelayCommand(CanExecute = nameof(CanExecuteOpenSelectedBookmarkUrl))]
-    private void OpenSelectedBookmarkUrl(CancelEventArgs cancelEventArgs)
-    {
-      Process.Start(new ProcessStartInfo
-      {
-        FileName = "opera",
-        Arguments = SelectedBookmark.HRef,
-        UseShellExecute = true
-      });
-    }
-
-    [RelayCommand]
-    void OpenSettings()
-    {
-      var result = _dialogService.ShowDialog(this, _settingsDialogViewModel);
-      if (result == true)
-      {
-        // Update theme if it changed
-        CurrentTheme = _settingsService.AppSettings.ReaderTheme;
-      }
-    }
-
-    [RelayCommand]
-    async Task OpenUpdate()
-    {
-      _updateDialogViewModel.Initialize();
-      var success = _dialogService.ShowDialog(this, _updateDialogViewModel);
-      if (success == true)
-      {
-        await Loaded();
-      }
-    }
-
-    [RelayCommand]
-    async Task Loaded()
-    {
-      Mouse.OverrideCursor = Cursors.Wait;
-      StatusBarText = $"Loading bookmarks..";
-
-      await BookmarkService.InitializeAsync(_settingsService.AppSettingsPath,
-                                            _settingsService.PinboardFileName);
-
-      BookmarkFileDateInfo = BookmarkService.BookmarkFileDateInfo;
-      LatestBookmarkDateInfo = BookmarkService.LatestBookmarkDateInfo;
-
-      Mouse.OverrideCursor = null;
-    }
-
-    private bool CanExecuteExit()
-    {
-      // Not allowed if shutdown is already in progress.
-      return !_isAlreadyShutdown;
-    }
-
-    [RelayCommand(CanExecute = nameof(CanExecuteExit))]
-    private void Exit(CancelEventArgs cancelEventArgs)
-    {
-      if (cancelEventArgs != null)
-      {
-        // The command was probably triggered by a WindowCLose event. We
-        // cancel that WindowsClose because shutdown is done manually
-        // depending on the MessageBox result below.
-        cancelEventArgs.Cancel = true;
-      }
-
-      MessageBoxResult result = MessageBoxResult.Yes;
-      if (_settingsService.AppSettings.AskBeforeAppExit)
-      {
-        result = _dialogService
-          .ShowMessageBox(this,
-                          "Do you really want to quit the application?",
-                          "Really exit?",
-                          MessageBoxButton.YesNo,
-                          MessageBoxImage.Warning);
-      }
-
-      if (result == MessageBoxResult.Yes)
-      {
-        // Shutdown() will trigger another invocation of this command.
-        // So we mark that the shutdown process has already started to
-        // block the re-invocation.
-        _isAlreadyShutdown = true;
-        Application.Current.Shutdown();
-      }
-    }
-
-    [RelayCommand]
-    void OpenTagFilter()
-    {
-      var success = _dialogService.ShowDialog(this, _filterByTagsDialogViewModel);
-      if (success == true)
-      {
-        // ??? await Filtered
-      }
-    }
-
-    IDialogService _dialogService;
-    ISettingsService _settingsService;
-    SettingsDialogViewModel _settingsDialogViewModel;
-    UpdateDialogViewModel _updateDialogViewModel;
-    FilterByTagsDialogViewModel _filterByTagsDialogViewModel;
-
-    bool _isAlreadyShutdown = false;
-  }
 }
